eee308bfdb0032e6316fb59d0dbb17efeecd7027
Austin Spinazze <austin.spinazze@perficient.com>
2020-04-02 13:31:01 -0500

    Added user places view and added map modal

---
+++ front-end/src/places/components/PlaceItem.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
+import "./PlaceItem.css";
+
+const PlaceItem = props => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+
+  const closeMapHandler = () => setShowMap(false);
+
+  return (
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        className="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+      >
+        <div className="mapContainer">
+          <h2>The Map!</h2>
+        </div>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              View On Map
+            </Button>
+            <Button inverse to={`/places/${props.id}`}>
+              Edit
+            </Button>
+            <Button danger>Delete</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
+  );
+};
+
+export default PlaceItem;
---
+++ front-end/src/shared/components/UIElements/Modal.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
+import BackDrop from "./Backdrop";
+import "./Modal.css";
+
+const ModalOverlay = props => {
+  // content is the content rendered in the modal
+  // className={`modal ${props.className}`} this allows multiple classNames to be used allowing reusability
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          // prettier-ignore
+          props.onSubmit ? props.onSubmit : (event => event.preventDefault)
+        }
+      >
+        <div className={`modal_content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+
+  return ReactDOM.createPortal(null, document.getElementById("modal-hook"));
+};
+
+const Modal = props => {
+  return (
+    <React.Fragment>
+      {props.show && <BackDrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        mountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </React.Fragment>
+  );
+};
+
+export default Modal;
