6f9b300a6ba91a350cd59bc910cfd6e1fd51973a
Austin Spinazze <austin.spinazze@perficient.com>
2020-04-02 13:53:08 -0500

    Fixed modal not closing bug and CSS mistakes

--- front-end/src/places/components/PlaceItem.js
+++ front-end/src/places/components/PlaceItem.js
@@ -18,17 +18,17 @@ const PlaceItem = props => {
         show={showMap}
         onCancel={closeMapHandler}
         header={props.address}
-        className="place-item__modal-content"
+        contentClass="place-item__modal-content"
         footerClass="place-item__modal-actions"
-        footer={<Button onClick={closeMapHandler}>Close</Button>}
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
-        <div className="mapContainer">
-          <h2>The Map!</h2>
+        <div className="map-container">
+          <h2>THE MAP!</h2>
         </div>
       </Modal>
       <li className="place-item">
         <Card className="place-item__content">
-          <div className="place-item">
+          <div className="place-item__image">
             <img src={props.image} alt={props.title} />
           </div>
           <div className="place-item__info">
--- front-end/src/shared/components/UIElements/Modal.js
+++ front-end/src/shared/components/UIElements/Modal.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
-import BackDrop from "./Backdrop";
+import Backdrop from "./Backdrop";
 import "./Modal.css";
 
 const ModalOverlay = props => {
-  // content is the content rendered in the modal
-  // className={`modal ${props.className}`} this allows multiple classNames to be used allowing reusability
   const content = (
     <div className={`modal ${props.className}`} style={props.style}>
       <header className={`modal__header ${props.headerClass}`}>
@@ -19,7 +17,7 @@ const ModalOverlay = props => {
           props.onSubmit ? props.onSubmit : (event => event.preventDefault())
         }
       >
-        <div className={`modal_content ${props.contentClass}`}>
+        <div className={`modal__content ${props.contentClass}`}>
           {props.children}
         </div>
         <footer className={`modal__footer ${props.footerClass}`}>
@@ -28,18 +26,17 @@ const ModalOverlay = props => {
       </form>
     </div>
   );
-
   return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
 };
 
 const Modal = props => {
   return (
     <React.Fragment>
-      {props.show && <BackDrop onClick={props.onCancel} />}
+      {props.show && <Backdrop onClick={props.onCancel} />}
       <CSSTransition
         in={props.show}
         mountOnEnter
-        mountOnExit
+        unmountOnExit
         timeout={200}
         classNames="modal"
       >
