dc8d0cd71f8414a616647d7e2da26466405c844c
AustinSpinazze <aspinazze94@gmail.com>
2021-07-02 16:58:32 -0500

    - Added axios package - Added error modal - Added loading spinner - Connected Signup logic to backend

--- back-end/app.js
+++ back-end/app.js
@@ -9,6 +9,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
 app.use("/api", placesRoutes);
 app.use("/api", userRoutes);
 
--- front-end/src/user/pages/Authenticate.js
+++ front-end/src/user/pages/Authenticate.js
@@ -1,8 +1,12 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, Fragment } from 'react';
+import axios from 'axios';
 
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpin from '../../shared/components/UIElements/LoadingSpinner';
+
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -13,9 +17,13 @@ import { useForm } from '../../shared/hooks/form-hook';
 import './Authenticate.css';
 
 const Authenticate = () => {
-    const auth = useContext(AuthContext);
+  const auth = useContext(AuthContext);
 
   const [isLoginMode, setIsLoginMode] = useState(true);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -55,27 +63,88 @@ const Authenticate = () => {
     setIsLoginMode(prevMode => !prevMode);
   };
 
-  const authSubmitHandler = event => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
-    auth.login();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    };
+
+    if (isLoginMode) {
+    } else {
+
+      try {
+
+        setIsLoading(true);
+
+        const body = {
+          name: formState.inputs.name.value,
+          username: formState.inputs.username.value,
+          email: formState.inputs.email.value,
+          password: formState.inputs.password.value
+        };
+
+        const responseData = await axios.post("http://localhost:5000/api/users/signup", body, config)
+          .then(response => {
+            return response.data;
+          });
+
+        console.log(responseData);
+
+        setIsLoading(false);
+
+        auth.login();
+        
+      } catch (error) {
+
+        console.log();
+
+        setIsLoading(false);
+
+        setError(error.response.data.message || "Something went wrong, please try again.");
+
+      }
+    }
+
+    setIsLoading(false);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  }
+
   return (
+    <Fragment>
+      <ErrorModal error={error} onClear={errorHandler}/>
     <Card className="authentication">
+      {isLoading && <LoadingSpin asOverlay/>}
       <h2>Login Required</h2>
       <hr />
       <form onSubmit={authSubmitHandler}>
         {!isLoginMode && (
-          <Input
-            element="input"
-            id="name"
-            type="text"
-            label="Your Name"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a name."
-            onInput={inputHandler}
-          />
+          <Fragment>
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a name."
+              onInput={inputHandler}
+            />
+            <Input
+              element="input"
+              id="username"
+              type="text"
+              label="Your Username"
+              validators={[VALIDATOR_MINLENGTH(5)]}
+              errorText="Please enter a username."
+              onInput={inputHandler}
+            />
+          </Fragment>
         )}
         <Input
           element="input"
@@ -103,6 +172,7 @@ const Authenticate = () => {
         SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
       </Button>
     </Card>
+    </Fragment>
   );
 };
 
